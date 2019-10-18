a25e0e8b010ed9bca68609f9f3ac257ef1b2e025
Rogelio <rogercp1234@yahoo.com>
2019-10-18 09:47:51 -0700

    created run method but bugs hiding

--- cpu.py
+++ cpu.py
@@ -89,3 +89,85 @@ class CPU:
         print()
 
     def run(self):
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+        MUL = 0b10100010
+        ADD = 0b10100000
+        AND = 0b10101000
+        POP = 0b01000110
+        LD  = 0b10000011
+        MOD = 0b10100100
+        PUSH = 0b01000101
+        CMP = 0b10100111
+        CALL = 0b01010000
+        RET = 0b00010001
+        JMP = 0b01010100
+        JNE = 0b01010110
+        JEQ = 0b01010101
+
+        while self.pc < len(self.ram):
+
+            command = self.ram[self.pc]
+            num_operands = (command & 0b11000000) >> 6
+
+            if num_operands == 1:
+                operand_a = self.ram_read(self.pc + 1)
+            elif num_operands == 2:
+                operand_a = self.ram_read(self.pc + 1)
+                operand_b = self.ram_read(self.pc + 2)
+
+            if command == HLT:
+                self.halt()
+            elif command == LDI:
+                self.reg[operand_a] = operand_b
+            elif command == PRN:
+                print(self.reg[operand_a])
+            elif command == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                print(self.reg[operand_a])
+            elif command == ADD:
+                self.alu("ADD", operand_, operand_b)
+            elif command == AND:
+                self.alu("AND", operand_a, operand_b)
+            elif command == LD:
+                self.reg[operand_a] = self.reg[operand_b]
+            elif command == MOD:
+                self.alu("MOD", operand_a, operand_b)
+            elif command == POP:
+                value = self.ram[self.reg[self.sp]]
+                self.reg[operand_a] = value
+                self.reg[self.sp] += 1
+            elif command == PUSH:
+                self.reg[self.sp] -= 1
+                value = self.reg[operand_a]
+                self.ram[self.reg[self.sp]] = value  
+            elif command == CMP:
+                self.alu("CMP", operand_a, operand_b)
+
+            if command == CALL:
+                return_addr = self.pc + 2
+                self.reg[self.sp] -= 1
+                self.ram[self.reg[self.sp]] = return_addr
+                regnum = self.ram[self.pc + 1]
+                subroutine_addr = self.reg[regnum]
+                self.pc = subroutine_addr
+                
+            elif command == RET:
+                return_addr = self.ram[self.reg[self.sp]]
+                self.reg[self.sp] += 1
+                self.pc = return_addr
+            elif command == JMP:
+                self.pc = self.reg[operand_a]
+            elif command == JNE:
+                if self.fl[self.e] == 0:
+                    self.pc = self.reg[operand_a]
+                else:
+                    self.pc += num_operands + 1
+            elif command == JEQ:
+                if self.fl[self.e] == 1:
+                    self.pc = self.reg[operand_a]
+                else:
+                    self.pc += num_operands + 1
+            else:
+                self.pc += num_operands + 1
